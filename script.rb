9eda1a3cd73f4b7c12502f45512cfe2a6f975918
Brian Davies <brian@briandavies.me>
2018-10-31 09:08:35 -0600

    fix buffer overflow

--- script.rb
+++ script.rb
@@ -5,25 +5,18 @@ CSV_PATH = "sce_export.csv"
 EXPORT_PATH = "saved_images"
 
 images = []
-count = 0
+
 CSV.foreach(CSV_PATH, row_sep: :auto, headers: true) do |row|
   urls = row[28].split(",")
+  part_number = row[155].gsub("@", "")
   urls.each_with_index do |url, index|
-    images << {
-      url: row[28],
-      part_number: row[155].gsub("@", "") + (index == 0 ? "" : "-" + index + 1)
-    }
-    count += 1
+    filename = part_number + (index == 0 ? "" : "-" + (index + 1).to_s)
+    file_extension = url.split(".").last
+    File.open("#{EXPORT_PATH}/#{filename}.#{file_extension}", 'wb') do |f|
+      f.write open(url).read
+    end
+    puts "Downloaded image: #{part_number}.#{file_extension}"
+    puts "    From #{url}"
   end
-
-  break if count > 10
 end
 
-images.each_with_index do |image, i|
-  file_extension = image[:url].split(".").last
-  File.open("#{EXPORT_PATH}/#{image[:part_number]}.#{file_extension}", 'wb') do |f|
-    f.write open(image[:url]).read
-  end
-  puts "Downloaded image ##{i}: #{image[:part_number]}.#{file_extension}"
-  puts "    From #{image[:url]}"
-end
